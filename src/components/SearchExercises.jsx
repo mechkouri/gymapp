63f2dbb04b826a871285ab837b740b8fd2791641
mechkouri <saidmechkouri@gmail.com>
2023-06-24 09:46:20 +0100

    create hero sections

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,38 @@
+import { Box, Button, Stack, TextField, Typography } from "@mui/material"
 
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent='center' p='29px'>
+      <Typography fontWeight={700} sx={{fontSize:{lg: '44px', xs:'30px'}}} mb='50px' textAlign='center'>
+        Awesome Exercises You <br /> should know
+      </Typography>
+      <Box position='relative' mb="72px">
+        <TextField 
+          sx={{ input:{ fontWeight:"700", border:"none", borderRadius:"4px"},
+                width:{lg:"1000px", xs:'350px'},
+                backgroundColor: '#fff',
+                borderRadius:'40px'
+              }}
+          height="76px"
+          value=''
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button className="search-btn"
+          sx={{ bgcolor:'#FF2625',
+                color:'#fff', 
+                textTransform:'none', 
+                width: {lg: '175px', xs:'80px'},
+                fontSize:{lg: '20px', xs:'14px'},
+                height:'56px'
+              }}  
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
   )
 }
 
