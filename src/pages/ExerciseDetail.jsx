3adc44db682b709c40224f001330300a86e58324
mechkouri <saidmechkouri@gmail.com>
2023-07-07 18:26:01 +0100

    create section Similar Target Muscle exercises

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -12,6 +12,8 @@ const ExerciseDetail = () => {
 
 const [exerciseDetail, setExerciseDetail] = useState({});
 const [exerciseVideos, setExerciseVideos] = useState([]);
+const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
+const [equipmentExercises, setEquipmentExercises] = useState([]);
 
 const {id} = useParams();
 
@@ -26,6 +28,11 @@ useEffect(() => {
     const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
     setExerciseVideos(exerciseVideosData.contents);
 
+    const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equimentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions);
+      setEquipmentExercises(equimentExercisesData);
 
 
   }
@@ -36,7 +43,7 @@ useEffect(() => {
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
     </Box>
   )
 }
